c5f4d80c7f6c83a8d8d05dd1ff740d8e41147d19
Musa17 <antonio.musulin@ericsson.com>
2022-08-05 09:09:08 +0200

    Adding routers in server project

--- server/index.js
+++ server/index.js
@@ -39,6 +39,22 @@ mongoose
   })
   .catch((err) => console.log(err));
 
+// Requiring routers from routers folder
+const userRouter = require("./router/userRouter");
+const conversationRouter = require("./router/conversationRouter");
+const messageRouter = require("./router/messageRouter");
+const teamsMessageRouter = require("./router/teamsMessageRouter");
+const teamsRouter = require("./router/teamsRouter");
+const notesRouter = require("./router/notesRouter");
+
+// using the routers
+app.use("/api/user", userRouter);
+app.use("/api/conversation", conversationRouter);
+app.use("/api/message", messageRouter);
+app.use("/api/teams", teamsRouter);
+app.use("/api/messageTeams", teamsMessageRouter);
+app.use("/api/notes", notesRouter);
+
 // Listening
 server.listen(process.env.PORT || 5000, () =>
   console.log("server is running on port 5000")
