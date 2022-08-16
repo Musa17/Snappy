56855fb5e783a9f148ba0a5c9a4774abf9dab9f1
Musa17 <antonio.musulin@ericsson.com>
2022-08-16 18:21:11 +0200

    Adding loading gif in Loading component

--- client/src/components/General/Loading/Loading.js
+++ client/src/components/General/Loading/Loading.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./Loading.css";
 import LoadingIcon from "../../../assets/images/Loading/SnappyLogo.png";
+import LoadingGif from "../../../assets/images/Loading/Loading.gif";
 
 // When the site is fetching data from the backend
 const Loading = (props) => {
   return (
     <div className="Loading">
       <img className="LoadingImage" src={LoadingIcon} alt="teams" />
+      <img className="LoadingGif" src={LoadingGif} alt="loadingGif" />
     </div>
   );
 };
